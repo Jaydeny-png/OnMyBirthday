865817e5910f691e99aae5400ba85176c3b48458
Jaydeny-png <jyau1020@gmail.com>
2024-02-02 11:51:38 -0500

    in class commit

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-function originalYear(){
+function originalYear() {
     document.getElementById('diffImage').src = 'images/me.jpg'
-    document.getElementById('change-paragraph').innerText='See that lil baby ghost? Thats me'
-    document.getElementById('changeDate').innerText= 'October 20th, 2007'
+    document.getElementById('change-paragraph').innerText = 'See that lil baby ghost? Thats me'
+    document.getElementById('changeDate').innerText = 'October 20th, 2007'
 }
-function year1803(){
+function year1803() {
     document.getElementById('diffImage').src = 'images/Louisiana-Purchase-size.webp'
-    document.getElementById('change-paragraph').innerText="The Louisiana Purchase was a historic event in which the United States acquired approximately 827,000 square miles of land west of the Mississippi River from France for $15 million in 1803. This doubled the size of the country and included land that eventually became parts of 15 states."
-    document.getElementById('changeDate').innerText= 'October 20th, 1803'
+    document.getElementById('change-paragraph').innerText = "The Louisiana Purchase was a historic event in which the United States acquired approximately 827,000 square miles of land west of the Mississippi River from France for $15 million in 1803. This doubled the size of the country and included land that eventually became parts of 15 states."
+    document.getElementById('changeDate').innerText = 'October 20th, 1803'
 }
-function year1964(){
+function year1964() {
     document.getElementById('diffImage').src = 'images/herbert.webp'
-    document.getElementById('change-paragraph').innerText="Herbert Hoover, the 31st President of the United States, died on October 20, 1964, at the age of 90 in New York City. The funeral for Hoover was held on October 23, 1964, and he was buried in West Branch, Iowa, next to his Presidential Library and Museum."
-    document.getElementById('changeDate').innerText= 'October 20th, 1964'
+    document.getElementById('change-paragraph').innerText = "Herbert Hoover, the 31st President of the United States, died on October 20, 1964, at the age of 90 in New York City. The funeral for Hoover was held on October 23, 1964, and he was buried in West Branch, Iowa, next to his Presidential Library and Museum."
+    document.getElementById('changeDate').innerText = 'October 20th, 1964'
 }
-function year1971(){
+function year1971() {
     document.getElementById('diffImage').src = 'images/Him.webp'
-    document.getElementById('change-paragraph').innerText='Kenneth Choi born October 20, 1971 is an Asian American actor.'
-    document.getElementById('changeDate').innerText= 'October 20th, 1971'
+    document.getElementById('change-paragraph').innerText = 'Kenneth Choi born October 20, 1971 is an Asian American actor.'
+    document.getElementById('changeDate').innerText = 'October 20th, 1971'
 }
-function year19712(){
+function year19712() {
     document.getElementById('diffImage').src = 'images/dogg.jpg'
-    document.getElementById('change-paragraph').innerText='Snoop Dogg, born on October 20, 1971, is an American rapper, singer, songwriter, producer, media personality, and actor. He is known for his relaxed style of rapping and love for cannabis culture.'
-    document.getElementById('changeDate').innerText= 'October 20th, 1971'
+    document.getElementById('change-paragraph').innerText = 'Snoop Dogg, born on October 20, 1971, is an American rapper, singer, songwriter, producer, media personality, and actor. He is known for his relaxed style of rapping and love for cannabis culture.'
+    document.getElementById('changeDate').innerText = 'October 20th, 1971'
 }
-function year1979(){
+function year1979() {
     document.getElementById('diffImage').src = 'images/reeds.webp'
-    document.getElementById('change-paragraph').innerText='John Krasinski (born October 20, 1979, Newton, Massachusetts, U.S.) American actor and director best known for playing paper salesman Jim Halpert in the NBC sitcom The Office.'
-    document.getElementById('changeDate').innerText= 'October 20th, 1979'
+    document.getElementById('change-paragraph').innerText = 'John Krasinski (born October 20, 1979, Newton, Massachusetts, U.S.) American actor and director best known for playing paper salesman Jim Halpert in the NBC sitcom The Office.'
+    document.getElementById('changeDate').innerText = 'October 20th, 1979'
 }
